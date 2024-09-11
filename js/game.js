f5b52e6643be3e7c004de2aa7cdc98930b9500e7
Mateo <matefernandezc@gmail.com>
2024-09-11 19:20:26 -0300

    test

--- js/game.js
+++ js/game.js
@@ -743,7 +743,7 @@ function iHeartYou(){
 	if(window.location.hash){
 		vtext.textContent = encryptString(decodeURIComponent(window.location.hash).substring(1));
 	}else{
-		vtext.textContent = "a lovely message from me to you <3";
+		vtext.textContent = "Yo te amo más igual <3";
 	}
 
 	setTimeout(function(){
